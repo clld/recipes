6bbd7b2635e0a4e8e18a1dde2b54ff0fcfe0aae6
Robert Forkel <xrotwang@googlemail.com>
2015-05-11 18:45:01 +0200

    removed dependency on clld

--- clld/make_icons.py
+++ clld/make_icons.py
@@ -1,19 +1,36 @@
 """script to create the default set of map marker icons distributed with clld."""
+import os
 import sys
 
-try:
-    import pyx
-    from pyx import bbox, unit, style, path, color, canvas, deco
-    # set the scale to 1/20th of an inch
-    unit.set(uscale=0.05, wscale=0.02, defaultunit="inch")
+import pyx
+from pyx import bbox, unit, style, path, color, canvas, deco
+# set the scale to 1/20th of an inch
+unit.set(uscale=0.05, wscale=0.02, defaultunit="inch")
 
-    linewidth = style.linewidth(1.2)
-except ImportError:  # pragma: no cover
-    pyx = False
+linewidth = style.linewidth(1.2)
 
-from pyramid.path import AssetResolver
 
-from clld.web.icon import ICONS
+ICONS = [
+    'ff6600',
+    'ffff00',
+    '000000',
+    'ffffff',
+    'dd0000',
+    '00ff00',
+    '00ffff',
+    'c00040',
+    '990099',
+    '8000ff',
+    'c000c0',
+    '800080',
+    'c000ff',
+    '800000',
+    '400040',
+    '00c000',
+    'ffc040',
+    '66ff33',
+    '400080',
+]
 
 
 def polygon(*points):  # pragma: no cover
@@ -48,15 +65,18 @@ def pyxColor(string):
 
 
 if __name__ == '__main__':  # pragma: no cover
+    out = sys.argv[1]
     if not pyx:
         sys.exit(1)
-    asset_resolver = AssetResolver()
     for icon in ICONS:
+        if len(icon) == 6:
+            icon = 'c' + icon
         c = canvas.canvas()
         c.draw(
-            shapes[icon.name[0]],
-            [deco.stroked([linewidth]), deco.filled([pyxColor(icon.name[1:])])])
+            shapes[icon[0]],
+            [deco.stroked([linewidth]), deco.filled([pyxColor(icon[1:])])])
         stream = c.pipeGS("pngalpha", resolution=20, bbox=bbox.bbox(0, 0, 40, 40))
-        with open(asset_resolver.resolve(icon.asset_spec).abspath(), 'wb') as fp:
+        with open(os.path.join(out, icon + '.png'), 'wb') as fp:
             fp.write(stream.read())
     sys.exit(0)
+
